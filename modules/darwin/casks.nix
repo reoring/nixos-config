af35ee89bd57876721ba32b1d3f0cb7bfb38dcf3
reoring <reoring@gmail.com>
2024-09-25 13:15:38 +0900

    chore: add modules

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,62 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+  "wezterm"
+  "tabby"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+  "zoom"
+  "loom"
+
+  # Utility Tools
+  "syncthing"
+  "cleanshot"
+  "bettertouchtool"
+  "one-switch"
+
+  # Entertainment Tools
+  "vlc"
+
+  # Productivity Tools
+  "raycast"
+  "obsidian"
+
+  # Browsers
+  "google-chrome"
+  "arc"
+
+  # Keyboard
+  "karabiner-elements"
+
+  # WindowManager
+  "nikitabobko/tap/aerospace"
+
+  # Password Management
+  "1password"
+
+  # Network
+  "tailscale"
+
+  # Input Methods
+  "aquaskk"
+
+  # You must install virtualbox manually for now
+  # "virtualbox"
+  "vagrant"
+
+  "font-meslo-lg-nerd-font"
+
+  "spotify"
+
+  "yubico-authenticator"
+  "yubico-yubikey-manager"
+
+  "raindropio"
+]
